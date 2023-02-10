1b0a2f53e5c198a33b1ca4b36e475467495d5b5f
Laurentius <laurentiuskevin44@gmail.com>
2023-02-10 16:49:30 +0700

    customize 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotFoundPage() {
   return (
     <div className="row justify-content-center px-3">
-      <div className="col-md-6 col-lg-4">
-        <h1 className="text-primary-custom text-center">
-          404 | Page not found
-        </h1>
+      <div className="col-md-6 col-lg-4 text-center">
+        <div className="display-4 text-primary-custom fw-bolder my-3">
+          OOPS !!!
+        </div>
+        <Image src="/icon/404.png" alt="404 icon" width={400} height={200} className="mt-3" />
+        <div className="h3 fw-bold" style={{color: "#14A2BA"}}>
+          The page you requested could not be found.
+        </div>
+
+        <Link href="/auth/sign-up">
+          <button type="button" className="btn px-5 btn-primary fw-bold btn-lg mt-2">
+            Create your Free pages now
+          </button>
+        </Link>
       </div>
     </div>
   );
