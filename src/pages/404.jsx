4b8b6eda4b759db3401e577274f0eb37f6ea2b40
Laurentius <laurentiuskevin44@gmail.com>
2023-02-14 08:03:40 +0700

    resize 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,16 +5,16 @@ export default function NotFoundPage() {
   return (
     <div className="row justify-content-center px-3">
       <div className="col-md-6 col-lg-4 text-center">
-        <div className="display-4 text-primary-custom fw-bolder my-3">
+        <div className="h1 text-primary-custom fw-bolder my-3">
           OOPS !!!
         </div>
         <Image src="/icon/404.png" alt="404 icon" width={400} height={200} className="mt-3" />
-        <div className="h3 fw-bold" style={{color: "#14A2BA"}}>
+        <div className="h5 fw-bold" style={{color: "#14A2BA"}}>
           The page you requested could not be found.
         </div>
 
         <Link href="/auth/sign-up">
-          <button type="button" className="btn px-5 btn-primary fw-bold btn-lg mt-2">
+          <button type="button" className="btn px-5 btn-primary fw-bold mt-2">
             Create your Free pages now
           </button>
         </Link>
